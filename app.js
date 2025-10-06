97310b75fa5da8f4b304f90d6603e8ea665c8a0a
Alexandra <alexandralee310@gmail.com>
2025-10-06 19:35:31 +0200

    Html fix. Dolt css och js.

---
+++ app.js
@@ -0,0 +1,43 @@
+/*
+const webDevelopmentQuiz = [
+    {
+        "title": "Fråga 1",
+        "question": "Vilket språk används för att styla webbsidor?",
+        "option1": "HTML",
+        "option2": "CSS",
+        "option3": "JavaScript",
+        "answer": "CSS"
+    },
+    {
+        "title": "Fråga 2",
+        "question": "Vilket av följande är ett ramverk för JavaScript?",
+        "option1": "React",
+        "option2": "PHP",
+        "option3": "Python",
+        "answer": "React"
+    },
+    {
+        "title": "Fråga 3",
+        "question": "Vilket attribut i HTML används för att länka en extern CSS-fil?",
+        "option1": "<script>",
+        "option2": "<link>",
+        "option3": "<style>",
+        "answer": "<link>"
+    },
+    {
+        "title": "Fråga 4",
+        "question": "Vilken HTML-tagg används för att skapa en ordnad lista?",
+        "option1": "<ol>",
+        "option2": "<ul>",
+        "option3": "<li>",
+        "answer": "<ol>"
+    },
+    {
+        "title": "Fråga 5",
+        "question": "Vilket av följande är inte ett programmeringsspråk?",
+        "option1": "Java",
+        "option2": "HTML",
+        "option3": "C++",
+        "answer": "HTML"
+    }] 
+    */
